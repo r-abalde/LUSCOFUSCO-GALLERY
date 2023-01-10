84e428c74c014ed4defb446699164ac35c3b137a
rosaliaperezabalde <Olatzbilbao0>
2023-01-10 19:00:52 +0100

    añade controller

---
+++ controllers/controllers.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+const ejs = require('ejs');
+//conexion con mongo
+const {MongoClient} = require("mongodb");
+app.set('view engine', 'ejs');
+app.use('/static', express.static('static'));
+const bodyParser = require('body-parser');
+path = require('path');
+app.use(express.json());
+app.use(express.static(path.join('../app')));
+app.use(bodyParser.urlencoded());
+
+/*Get for full online gallery */
+const galleryART = (req, res) => {
+    var MongoClient = require('mongodb').MongoClient;
+    const url = "mongodb+srv://ud:ud@cluster0.szg5vgf.mongodb.net/?retryWrites=true&w=majority";
+    MongoClient.connect(url, function (err, client) {
+        if (err) throw err;
+        var db = client.db('luscofusco');
+        db.collection('artgallery').find({}).toArray(function (findErr, art) {
+            if (findErr) throw findErr;
+            /*
+            for (let i = 0; i < art.length; i++) {
+                console.log("tres");
+                console.log(art[i]);
+            }*/
+            res.render('gallery.ejs', {artList: art});
+
+            client.close();
+        });
+    });
+
+};
+
+/*Get with only the pieces owned by the gallery */
+const shopART = (req, res, next) => {
+    var MongoClient = require('mongodb').MongoClient;
+    const url = "mongodb+srv://ud:ud@cluster0.szg5vgf.mongodb.net/?retryWrites=true&w=majority";
+    MongoClient.connect(url, function (err, client) {
+        if (err) throw err;
+        var db = client.db('luscofusco');
+        db.collection('artgallery').find({ownedBy: "Luscofusco"}).toArray(function (findErr, art) {
+            if (findErr) throw findErr;
+
+            res.render('galleryShop.ejs', {artList: art});
+
+            client.close();
+        });
+    });
+};
+
+/*Page with search results */
+const searchART = (req, res, next) => {
+    var MongoClient = require('mongodb').MongoClient;
+    const url = "mongodb+srv://ud:ud@cluster0.szg5vgf.mongodb.net/?retryWrites=true&w=majority";
+    MongoClient.connect(url, function (err, client) {
+        if (err) throw err;
+        var db = client.db('luscofusco');
+        db.collection('artgallery').find({$or: [ {title: req.body.nameArt},{author: req.body.nameArt}]}).toArray(function (findErr, art) {
+            if (findErr) throw findErr;
+
+            res.render('galleryResults.ejs', {artList: art});
+
+            client.close();
+        });
+    });
+};
+
+
+/*Flask functions*/
+/*
+const loginARTPOST = (req, res) => {
+    var options = { method: 'POST',
+        url: 'http://127.0.0.1:5000/login'};
+    return request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        res.sendFile('login.html',{root: 'views'});
+    });
+};
+const loginARTGET = (req, res) => {
+    request('http://127.0.0.1:5000/login', function (error, response, body) {
+        console.error('error:', error); // Print the error
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        res.send(body); //Display the response on the website
+    });
+};
+
+const logoutART = (req, res) => {
+    request('http://127.0.0.1:5000/logout', function (error, response, body) {
+        console.error('error:', error); // Print the error
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        res.send(body); //Display the response on the website
+    });
+};
+
+const registerARTGET = (req, res) => {
+    var options = { method: 'GET',
+        url: 'http://127.0.0.1:5000/register'};
+    console.log("get")
+    return request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        res.sendFile( 'register.html',{root: 'views'});
+    });
+
+};
+const registerARTPOST = (req, res) => {
+    var options = { method: 'POST',
+        url: 'http://127.0.0.1:5000/register'};
+    console.log("post")
+    return request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        res.sendFile('register.html',{root: 'views'});
+    });
+};
+
+const indexART = (req, res) => {
+    var options = { method: 'GET',
+        url: 'http://127.0.0.1:5000/index'};
+    console.log("get")
+    return request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        res.sendFile( 'index.html',{root: 'templates'});
+    });
+};*/
+
+//Exportar los modulos para ser recibidos por routes.js
+module.exports = {
+    galleryART,
+    searchART,
+    shopART
+};
